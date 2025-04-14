665a8f869d979e41c0518ffd0cf9e70761fcb7f7
Giuseppe Filippin <giuseppe.ferreira@pucpr.edu.br>
2025-04-14 14:12:43 -0300

    bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-// App.jsx
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DataList from "./components/DataList";
--- frontend/src/components/MoreInfo.jsx
+++ frontend/src/components/MoreInfo.jsx
@@ -1,4 +1,3 @@
-// components/MoreInfo.jsx
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
